fc2540cec7f4f2070d416bb3fad5286efe2e6049
sweepingmonk <gaofeng691027@163.com>
2020-04-10 18:50:41 +0800

    新增

--- demo6.py
+++ demo6.py
@@ -1,4 +1,4 @@
-#coding=utf-8
+# coding=utf-8
 """
 ************************************
 --- Author:IT扫地僧 我是高老师
@@ -9,22 +9,22 @@
 """
 from selenium import webdriver
 import time
-chrome_driver=u'C:\\Users\\huawei\\Desktop\\python最新教案\\chromedriver_win32\chromedriver.exe'
+
+chrome_driver = u'C:\\Users\\huawei\\Desktop\\python最新教案\\chromedriver_win32\chromedriver.exe'
 driver = webdriver.Chrome(executable_path=chrome_driver)
-#open the url
+# open the url
 driver.get(r'C:\Users\huawei\Desktop\python最新教案\selenium教程\html\a.html')
 driver.maximize_window()
 driver.implicitly_wait(3)
 driver.find_element_by_id('alert').click()
-alert=driver.switch_to.alert
+alert = driver.switch_to.alert
 print(alert.text)
 time.sleep(3)
 alert.accept()
 time.sleep(3)
 driver.find_element_by_id('confirm').click()
-confirm=driver.switch_to.alert
+confirm = driver.switch_to.alert
 time.sleep(3)
 confirm.dismiss()
 time.sleep(3)
 driver.quit()
-
